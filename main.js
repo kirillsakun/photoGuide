78eb90825e84d4c57479c21e9eeeb519889d1939
Kirill Sakun <kirillsakun03@gmail.com>
2020-03-14 10:09:44 +0300

    team section added (without video)

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
+
 window.addEventListener('scroll', function() {
-  parallaxId('hero', 0.5, '-80px', '- 48vh');
+  parallaxId('hero', 0.5, '-80px', '- 1px');
+  parallaxId('team', 0.3, '0px', '- 1px');
 });
 
 window.FontAwesomeConfig = {
   searchPseudoElements: true,
 };
 
-function parallaxId(el, koeficent, x, y) {
-  if (!!document.getElementById(el)) {
-    let scrolled = window.pageYOffset;
-    document.getElementById(el).style.backgroundPosition = `${x} calc(${0 -
-      scrolled * koeficent}px ${y})`;
+function parallaxId(targetID, koeficent, x, y) {
+  const target = document.getElementById(targetID);
+
+  let targetPosition = {
+      top: window.pageYOffset + target.getBoundingClientRect().top,
+      bottom: window.pageYOffset + target.getBoundingClientRect().bottom,
+    },
+    windowPosition = {
+      top: window.pageYOffset,
+      bottom: window.pageYOffset + document.documentElement.clientHeight,
+    };
+
+  if (targetPosition.bottom > windowPosition.top && targetPosition.top < windowPosition.bottom) {
+    target.style.backgroundPosition = `${x} calc(${0 - (windowPosition.bottom - targetPosition.top) * koeficent}px ${y})`;
+    console.log('Вы видите элемент ' + targetID);
+  } else {
+    console.clear();
   }
 }
+
 const modal = document.getElementById('modal');
 const pics = document.getElementsByClassName('pic');
 
 modal.onclick = function() {
   modal.style.transform = 'scale(0)';
 };
-
 for (let i = 0; i < pics.length; i++) {
   pics[i].onclick = function() {
     modal.lastElementChild.src = pics[i].lastElementChild.src;
@@ -34,9 +48,7 @@ for (let i = 0; i < forms.length; i++) {
     addEventListener('focusin' && 'focusout', () => {
       if (inputs[j].value !== '') {
         inputs[j].className = 'filled';
-      }
-      else 
-      inputs[j].className = '';
+      } else inputs[j].className = '';
     });
   }
 }
